c44128c5b7da6400a315803305050eba2007fd5c
Sebastian Fix <sebastian.fix@onicial.ch>
2019-12-10 18:27:55 +0000

    Implemented Version

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,17 +1,19 @@
 <div class="md:w-1/2 md:mx-auto mt-24">
-<footer class="flex flex-wrap p-6">
-    <div class="w-full">
-        <div class="py-6">
-            <div class="text-black font-bold text-xs tracking-tight">
-                <a href="{{ route('login') }}" class="mr-2">Authorize</a>
-            </div>
-            <div class="py-1 text-xs tracking-tight font-light">
+    <footer class="flex flex-wrap p-6">
+        <div class="w-full">
+            <div class="py-6">
+                <div class="text-black font-bold text-xs tracking-tight">
+                    <a href="{{ route('login') }}" class="mr-2">Authorize</a>
+                </div>
+                <div class="py-1 text-xs tracking-tight font-light">
                 <span class="mr-2">
-                    <i class="fad fa-copyright"></i>
-                    <a target="_blank" href="{{ route('debug.patients.index') }}"><span class="">Copyright {{ date('Y') }}</span></a>
+                    <a target="_blank" href="{{ route('debug.patients.index') }}">
+                        <i class="fad fa-copyright"></i>
+                    </a>
+                   <span class="" title="{{ config('digibp.version') }}">Copyright {{ date('Y') }}</span>
                 </span>
+                </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
 </div>
